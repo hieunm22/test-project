a5d3cfe7d4d5b70fe548a529483e9cb15d972fa0
Minh Hieu <hieunm1987@gmail.com>
2022-07-01 14:56:19 +0700

    init

---
+++ testing-react-hooks/src/Counter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+function Counter() {
+  const [counter, setCounter] = useState(0)
+
+  const decrease = () => {
+    setCounter(counter - 1)
+  }
+
+  const increase = () => {
+    setCounter(counter + 1)
+  }
+
+  return (
+    <div className="counter">
+      <button id="increment-btn" onClick={increase}>Increase</button>
+      <button id="decrement-btn" onClick={decrease}>Decrease</button>
+      <div id="counter">Counter:
+        <span id="counter-value">{counter}</span>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
---
+++ testing-react-hooks/src/Counter.test.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { mount, configure } from 'enzyme'
+import Counter from './Counter'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+configure({adapter: new Adapter()});
+
+describe('Counter Testing', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = mount(<Counter />)
+  })
+
+  test('test click increase button', () => {
+    const incrementButton = wrapper.find('#increment-btn')
+    incrementButton.simulate('click')
+    incrementButton.simulate('click')
+    const txt = wrapper.find('#counter-value').text()
+    expect(txt).toBe('2')
+    expect(txt > 0).toBe(true)
+  })
+
+  test('test click decrease button', () => {
+    const decrementButton = wrapper.find('#decrement-btn')
+    decrementButton.simulate('click')
+    decrementButton.simulate('click')
+    decrementButton.simulate('click')
+    expect(wrapper.find('#counter-value').text()).toBe('-3')
+  })
+
+  test('test click both butotn', () => {
+    const decrementButton = wrapper.find('#decrement-btn')
+    decrementButton.simulate('click')
+    decrementButton.simulate('click')
+    expect(wrapper.find('#counter-value').text()).toBe('-2')
+    const incrementButton = wrapper.find('#increment-btn')
+    incrementButton.simulate('click')
+    incrementButton.simulate('click')
+    incrementButton.simulate('click')
+    incrementButton.simulate('click')
+    incrementButton.simulate('click')
+    incrementButton.simulate('click')
+    expect(wrapper.find('#counter-value').text()).toBe('4')
+  })
+})
